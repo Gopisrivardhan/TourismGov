bbb4ab6a58501414279600051ee2db7e5508c037
yaswanth <yallapuyas@gmail.com>
2026-05-11 11:36:05 +0530

    Event web Pages added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AdminEvents from './pages/AdminEvents';
+import TouristEvents from './pages/TouristEvents';
 function App() {
   return (
     <Router>
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminEvents />} />
+        <Route path="/tourist" element={<TouristEvents />} />
       </Routes>
     </Router>
   );
